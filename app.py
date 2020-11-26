2c60c4f1ceba4b57eb1e5c489552e6cc3864f560
Fred-Anziko <74971350+Fred-Anziko@users.noreply.github.com>
2020-11-26 15:52:55 +0300

    a

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 import functools
 from flask import Blueprint
-#from flask_session import Session
+from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
