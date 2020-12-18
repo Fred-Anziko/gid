6528b5e8d19677c1d1c990a7d533b1088178c01c
Fred-Anziko <74971350+Fred-Anziko@users.noreply.github.com>
2020-12-18 20:18:17 +0300

    changed gidmodal,gidapp
    DB2 connection successful

--- gidapp.py
+++ gidapp.py
@@ -11,6 +11,7 @@ import gidmodal
 from gidmodal import gidconnection
 import waitress
 import psycopg2
+import ibm_db
 
 
 
@@ -88,40 +89,22 @@ def register():
         password = request.form["password"]
         personnel_gps=50000
         date_of_reg=datetime.datetime.now()
-        error=None
-
-        if (
-            gidcursor.execute("SELECT Personnel_Id_No FROM personnels WHERE Personnel_User_Name =(%s)", (user_name)).fetchone()
-            is not None
-        ):
+        """acessing DB2 database to check if the username exists"""
+        check_userame_sql="SELECT Personnel_Id_No FROM personnels WHERE Personnel_User_Name =?"
+        check_userame_sql_prepared=ibm_db.prepare(gidconnection,check_userame_sql)
+        check_userame_sql_parameters=user_name
+        personnel_username_checked=ibm_db.execute(check_userame_sql_prepared,check_userame_sql_parameters).ibm_db.fetch_assoc()
+        if personnel_username_checked is not None:
 
             return f"<h2>Sorry! Username <em>{user_name}</em> is already taken,select another username and</h2><a href = '/gidregister'>" + "<strong>Register again</strong></a>"
 
         else:
             """ the name is not in system, store it in the database and go to
             the login page"""
-            gidcursor.execute(
-                "INSERT INTO personnels (Personnel_First_Name,Personnel_Last_Name,Personnel_User_Name,Personnel_Password,Personnel_Tel_No,Personnel_Email_address,Personnel_Country,Personnel_City_Of_Residence,Personnel_Qualifications,Personnel_Experiences,Personnel_Skills,Personnel_Gps_Location,Personnel_Date_Of_Registration) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                (
-
-                firstname,
-                lastname,
-                user_name,
-                generate_password_hash(password),
-                telephone,
-                email,
-                country,
-                city,
-                qualification,
-                experiences,
-                skills,
-                personnel_gps,
-                date_of_reg
-                )
-            )
-            gidconnection.commit()
-
-            
+            personnel_register_sql="INSERT INTO personnels (Personnel_First_Name,Personnel_Last_Name,Personnel_User_Name,Personnel_Password,Personnel_Tel_No,Personnel_Email_address,Personnel_Country,Personnel_City_Of_Residence,Personnel_Qualifications,Personnel_Experiences,Personnel_Skills,Personnel_Gps_Location,Personnel_Date_Of_Registration) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+            personnel_register_sql_prepared=ibm_db.prepare(gidconnection,personnel_register_sql)
+            personnel_sql_parameters=firstname,lastname,user_name,generate_password_hash(password),telephone,email,country,city,qualification,experiences,skills,personnel_gps,date_of_reg
+            ibm_db.execute(personnel_register_sql_prepared,personnel_sql_parameters)
             return redirect("/gidlogin")
     else:
         return render_template("register.html")
@@ -145,37 +128,23 @@ def employer_register():
         ePassword = request.form["Password"]
         eEmployer_Gps=50000
         eDate_of_Reg=datetime.datetime.now()
-        eError=None
+        
+        employer_check_sql="SELECT Employer_Id_No FROM employers WHERE Employer_User_Name = ?"
+        employer_check_sql_prepared=ibm_db.prepare(gidconnection,employer_check_sql)
+        employer_check_sql_parameters=eUserName
+        employer_username_checked=ibm_db.execute(employer_check_sql_prepared,employer_check_sql_parameters).ibm_db.fetch_assoc()
 
-        if (
-            gidcursor.execute("SELECT Employer_Id_No FROM employers WHERE Employer_User_Name = (%s)", (eUserName)).fetchone()
-            is not None
-        ):
+        if employer_username_checked is not None:
 
             return f"<h2>Sorry! Username <em>{eUserName}</em> is already taken,select another username and</h2><a href = '/employergidregister'>" + "<strong>Register again</strong></a>"
 
         else:
             # the name is not in system, store it in the database and go to
             # the login page
-            gidcursor.execute(
-                "INSERT INTO employers (Employer_First_Name,Employer_Last_Name,Employer_User_Name,Employer_Password,Employer_Tel_No,Employer_Email_Address,Employer_Country,Employer_City_Of_Residence,Employer_Gps_Location,Employer_Date_Of_Registration) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                (
-
-                efirstName,
-                elastName,
-                eUserName,
-                generate_password_hash(ePassword),
-                eTelephone,
-                eEmail,
-                eCountry,
-                eCity,
-                eEmployer_Gps,
-                eDate_of_Reg
-                )
-            )
-            gidconnection.commit()
-
-            
+            employer_register_sql="INSERT INTO employers (Employer_First_Name,Employer_Last_Name,Employer_User_Name,Employer_Password,Employer_Tel_No,Employer_Email_Address,Employer_Country,Employer_City_Of_Residence,Employer_Gps_Location,Employer_Date_Of_Registration) VALUES (?,?,?,?,?,?,?,?,?,?)"
+            employer_register_sql_prepared=ibm_db.prepare(gidconnection,employer_register_sql)
+            employer_register_sql_parameters=efirstName,elastName,eUserName,generate_password_hash(ePassword),eTelephone,eEmail,eCountry,eCity,eEmployer_Gps,eDate_of_Reg
+            ibm_db.execute(employer_register_sql_prepared,employer_register_sql_parameters)
             return redirect("/employergidlogin")
     else:
         return render_template("employerregistry.html")
@@ -204,19 +173,19 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        error = None
-        user = gidcursor.execute(
-            "SELECT * FROM personnels WHERE Personnel_User_Name =(%s)", (username)
-        ).fetchone()
+        user_sql ="SELECT * FROM personnels WHERE Personnel_User_Name =?"
+        user_sql_prepared=ibm_db.prepare(gidconnection,user_sql)
+        user_sql_parameters=username
+        user_personnel=ibm_db.execute(user_sql_prepared,user_sql_parameters).ibm_db.fetch_assoc()
 
-        if user is None:
+        if user_personnel is None:
             return "<h2>Incorrect username, Sorry you have not been logged in!</h2><br><a href = '/gidlogin'>" + "<strong>Login again</strong></a>"
-        elif not check_password_hash(user[4], password):
+        elif not check_password_hash(user_personnel[4], password):
             return "<h2>Incorrect password, Sorry you have not been logged in!</h2><br><a href = '/gidlogin'>" + "<strong>Login again</strong></a>"
 
         else:
             # store the user id in a new session and return to the index
-            session["user_id"] = user[0]
+            session["user_id"] = user_personnel[0]
             return redirect("/personnelhomepage")
     else:
         return render_template("gidlogin.html")
@@ -229,19 +198,19 @@ def employer_login():
     if request.method == "POST":
         euserName = request.form["userName"]
         epassWord = request.form["PassWord"]
-        error = None
-        userEmployer = gidcursor.execute(
-            "SELECT * FROM employers WHERE Employer_User_Name = (%s)", (euserName)
-        ).fetchone()
+        user_employer_sql="SELECT * FROM employers WHERE Employer_User_Name = ?"
+        user_employer_sql_prepared=ibm_db.prepare(gidconnection,user_employer_sql)
+        user_employer_sql_parameters=euserName
+        user_employer=ibm_db.execute(user_employer_sql_prepared,user_employer_sql_parameters)
 
-        if userEmployer is None:
+        if user_employer is None:
             return "<h2>Incorrect username, Sorry you have not been logged in!</h2><br><a href = '/employergidlogin'>" + "<strong>Login again</strong></a>"
-        elif not check_password_hash(userEmployer[4], epassWord):
+        elif not check_password_hash(user_employer[4], epassWord):
             return "<h2>Incorrect password, Sorry you have not been logged in!</h2><br><a href = '/employergidlogin'>" + "<strong>Login again</strong></a>"
 
         else:
             # store the user id in a new session and redirect to employers page
-            session["employer_user_id"] = userEmployer[0]
+            session["employer_user_id"] = user_employer[0]
             return redirect("/employerhomepage")
     else:
         return render_template("employergidlogin.html")
@@ -263,17 +232,18 @@ def employer_logout():
 @login_as_employer_required
 def employerhomepage():
     """employerhomepage.html."""
-    loggedinemployer=session["employer_user_id"]
-    useremployer = gidcursor.execute(
-            "SELECT * FROM employers WHERE Employer_Id_No = (%s)", (loggedinemployer)
-        ).fetchone()
-    username=useremployer[3]
+    loggedin_employer=session["employer_user_id"]
+    employer_loggedin_sql="SELECT * FROM employers WHERE Employer_Id_No =?"
+    employer_loggedin_sql_prepared=ibm_db.prepare(gidconnection,employer_loggedin_sql)
+    employer_loggedin_sql_parameters=loggedin_employer
+    user_employer=ibm_db.execute(employer_loggedin_sql_prepared,employer_loggedin_sql_parameters)
+    username=user_employer[3]
     username1=username[0]
-    taskStatus = gidcursor.execute(
-        "SELECT * FROM tasks WHERE Employer_Identity=(%s)"
-        " ORDER BY Task_Date_Of_Post DESC", (loggedinemployer)
-    ).fetchall()
-    return render_template("gidemployerhomepage.html",username1=username1,username=username,taskStatus=taskStatus)
+    task_sql="SELECT * FROM tasks WHERE Employer_Identity=?" ," ORDER BY Task_Date_Of_Post DESC"
+    task_sql_prepared=ibm_db.prepare(gidconnection,task_sql)
+    task_sql_parameters=loggedin_employer
+    task_status=ibm_db.execute(task_sql_prepared,task_sql_parameters)
+    return render_template("gidemployerhomepage.html",username1=username1,username=username,taskStatus=task_status)
 
 @gidapp.route("/gidprofile", methods=["GET", "POST"])
 @login_required
@@ -293,9 +263,9 @@ def doajob():
 @login_required
 def personnelhomepage():
     """personnelhomepage.html"""
-    loggedinuser=session["user_id"]
+    loggedin_user=session["user_id"]
     user = gidcursor.execute(
-            "SELECT * FROM personnels WHERE Personnel_Id_No = (%s)", (loggedinuser)
+            "SELECT * FROM personnels WHERE Personnel_Id_No = ?", (loggedinuser)
         ).fetchone()
     userfirstname1=user[1]
     userlastname=user[2]
--- gidmodal.py
+++ gidmodal.py
@@ -2,15 +2,21 @@ import ibm_db
 import psycopg2
 
 ########MODAL/DATABASE########BEGINS
-#connection to the PostgreSQL giddatabase.db file
-
-gidconnection= ibm_db.connect("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-15.services.eu-gb.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=wtg44875;PWD=x0@jrhzljmp7996f;","","")
+gidconnection=False
+#connection to the DB2 database for gidweb on IBM cloud
+try:
+    gidconnection= ibm_db.pconnect("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-15.services.eu-gb.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=wtg44875;PWD=x0@jrhzljmp7996f;","","")
+except:
+    print( "No connection:" , ibm_db.conn_errormsg())
+else:
+    print ("Connection established.")
 #except:
 #     gidconnection=psycopg2.connect(dbname="giddatabase", user="postgres", password="Afeku demetrio2020")
 ##creat Database tables in giddatabase begins##
 #gidcursor=gidconnection.cursor()
-# Create employers table
-"""{
+
+
+"""DB2 CONNECTION PARAMETERS ON IBM CLOUD FOUNDRY
   "db": "BLUDB",
   "dsn": "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-15.services.eu-gb.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=wtg44875;PWD=x0@jrhzljmp7996f;",
   "host": "dashdb-txn-sbox-yp-lon02-15.services.eu-gb.bluemix.net",
@@ -26,24 +32,30 @@ gidconnection= ibm_db.connect("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-
   "ssljdbcurl": "jdbc:db2://dashdb-txn-sbox-yp-lon02-15.services.eu-gb.bluemix.net:50001/BLUDB:sslConnection=true;",
   "uri": "db2://wtg44875:x0%40jrhzljmp7996f@dashdb-txn-sbox-yp-lon02-15.services.eu-gb.bluemix.net:50000/BLUDB",
   "username": "wtg44875"
-}"""
+"""
+"""Create employers table"""
 try:
-    gidemployee_sql="CREATE TABLE employer(Employer_Id_No INTEGER PRIMARY KEY, Employer_First_Name TEXT,Employer_Last_Name TEXT,Employer_User_Name TEXT UNIQUE NOT NULL,Employer_Password TEXT NOT NULL,Employer_Tel_No TEXT,Employer_Email_Address TEXT,Employer_Country TEXT,Employer_City_Of_Residence TEXT,Employer_Gps_Location REAL NOT NULL,Employer_Date_Of_RegistrationTIMESTAMP NOT NULL)"
+    gidemployee_sql="CREATE TABLE employer(Employer_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1), Employer_First_Name VARCHAR(25),Employer_Last_Name VARCHAR(25),Employer_User_Name VARCHAR(25) UNIQUE NOT NULL,Employer_Password VARCHAR(25) NOT NULL,Employer_Tel_No VARCHAR(25),Employer_Email_Address VARCHAR(25),Employer_Country CHAR,Employer_City_Of_Residence VARCHAR(25),Employer_Gps_Location DECIMAL(10,2) NOT NULL,Employer_Date_Of_Registration TIMESTAMP NOT NULL)"
     ibm_db.exec_immediate(gidconnection,gidemployee_sql)
 except:
-    pass
+    print( "Transaction couldn't be completed:" , ibm_db.stmt_errormsg())
+else:
+    print ("Transaction complete.")
 
-# Create tasks table for holding task posting and updating details
+
+"""Create tasks table for holding task posting and updating details"""
 try:
-    gidtasks_sql="CREATE TABLE tasks(Task_Id_No INTEGER PRIMARY KEY,Employer_Identity INTEGER NOT NULL,Task_Category TEXT,Task_Name TEXT,Task_Description TEXT,Task_Requirements TEXT,Task_Gps_Location REAL,Task_Budget_Amount REAL,Task_Budget_Currency TEXT,Task_Date_Of_Post TIMESTAMP NOT NULL,Task_Deadline DATE, FOREIGN KEY (Employer_Identity) REFERENCES employers (Employer_Id_No))"
+    gidtasks_sql="CREATE TABLE tasks(Task_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),Employer_Identity INTEGER NOT NULL,Task_Category VARCHAR(25),Task_Name VARCHAR(25),Task_Description VARCHAR(250),Task_Requirements VARCHAR(250),Task_Gps_Location DECIMAL(10,2),Task_Budget_Amount DECIMAL(10,2),Task_Budget_Currency VARCHAR(25),Task_Date_Of_Post TIMESTAMP NOT NULL,Task_Deadline DATE)" # FOREIGN KEY (Employer_Identity) REFERENCES employers (Employer_Id_No))"
     ibm_db.exec_immediate(gidconnection,gidtasks_sql)
 except:
-    pass
-
-# Create personnel table
+    print( "Transaction couldn't be completed:" , ibm_db.stmt_errormsg())
+else:
+    print ("Transaction complete.")
+"""Create personnel table"""
 try:
-    gidpersonnels_sql="CREATE TABLE personnels(Personnel_Id_No INTEGER PRIMARY KEY,Personnel_First_Name TEXT,Personnel_Last_Name TEXT,Personnel_User_Name TEXT UNIQUE NOT NULL, Personnel_Password TEXT NOT NULL, Personnel_Tel_No TEXT, Personnel_Email_address TEXT,Personnel_Country TEXT,Personnel_City_Of_Residence TEXT, Personnel_Qualifications TEXT,Personnel_Experiences TEXT,Personnel_Skills TEXT,Personnel_Gps_Location REAL NOT NULL,Personnel_Date_Of_Registration TIMESTAMP NOT NULL)"
+    gidpersonnels_sql="CREATE TABLE personnels(Personnel_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),Personnel_First_Name VARCHAR(25),Personnel_Last_Name VARCHAR(25),Personnel_User_Name VARCHAR(25) UNIQUE NOT NULL, Personnel_Password VARCHAR(25) NOT NULL, Personnel_Tel_No VARCHAR(25), Personnel_Email_address VARCHAR(25),Personnel_Country VARCHAR(25),Personnel_City_Of_Residence VARCHAR(25), Personnel_Qualifications VARCHAR(250),Personnel_Experiences VARCHAR(250),Personnel_Skills VARCHAR(25),Personnel_Gps_Location DECIMAL(10,2) NOT NULL,Personnel_Date_Of_Registration TIMESTAMP NOT NULL)"
     ibm_db.exec_immediate(gidconnection,gidpersonnels_sql)
 except:
-    pass
-##creat Database tables in giddatabase ends##
+    print( "Transaction couldn't be completed:" , ibm_db.stmt_errormsg())
+else:
+    print ("Transaction complete.")
