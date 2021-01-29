3014de557ff53e7de459acd852d0ca158bdb4160
Fred-Anziko <74971350+Fred-Anziko@users.noreply.github.com>
2021-01-29 13:45:31 +0300

    Update gidmodal.py

--- gidmodal.py
+++ gidmodal.py
@@ -33,7 +33,7 @@ else:
   "uri": "db2://wtg44875:x0%40jrhzljmp7996f@dashdb-txn-sbox-yp-lon02-15.services.eu-gb.bluemix.net:50000/BLUDB",
   "username": "wtg44875"
 """
-"""Create employers table"""
+"""Create employers table
 try:
     gidemployee_sql="CREATE TABLE employer(Employer_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1), Employer_First_Name VARCHAR(25),Employer_Last_Name VARCHAR(25),Employer_User_Name VARCHAR(25) UNIQUE NOT NULL,Employer_Password VARCHAR(25) NOT NULL,Employer_Tel_No VARCHAR(25),Employer_Email_Address VARCHAR(25),Employer_Country CHAR,Employer_City_Of_Residence VARCHAR(25),Employer_Gps_Location DECIMAL(10,2) NOT NULL,Employer_Date_Of_Registration TIMESTAMP NOT NULL)"
     ibm_db.exec_immediate(gidconnection,gidemployee_sql)
@@ -43,7 +43,7 @@ else:
     print ("Transaction complete.")
 
 
-"""Create tasks table for holding task posting and updating details"""
+Create tasks table for holding task posting and updating details
 try:
     gidtasks_sql="CREATE TABLE tasks(Task_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),Employer_Identity INTEGER NOT NULL,Task_Category VARCHAR(25),Task_Name VARCHAR(25),Task_Description VARCHAR(250),Task_Requirements VARCHAR(250),Task_Gps_Location DECIMAL(10,2),Task_Budget_Amount DECIMAL(10,2),Task_Budget_Currency VARCHAR(25),Task_Date_Of_Post TIMESTAMP NOT NULL,Task_Deadline DATE)" # FOREIGN KEY (Employer_Identity) REFERENCES employers (Employer_Id_No))"
     ibm_db.exec_immediate(gidconnection,gidtasks_sql)
@@ -51,11 +51,11 @@ except:
     print( "Transaction couldn't be completed:" , ibm_db.stmt_errormsg())
 else:
     print ("Transaction complete.")
-"""Create personnel table"""
+Create personnel table
 try:
     gidpersonnels_sql="CREATE TABLE personnels(Personnel_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),Personnel_First_Name VARCHAR(25),Personnel_Last_Name VARCHAR(25),Personnel_User_Name VARCHAR(25) UNIQUE NOT NULL, Personnel_Password VARCHAR(25) NOT NULL, Personnel_Tel_No VARCHAR(25), Personnel_Email_address VARCHAR(25),Personnel_Country VARCHAR(25),Personnel_City_Of_Residence VARCHAR(25), Personnel_Qualifications VARCHAR(250),Personnel_Experiences VARCHAR(250),Personnel_Skills VARCHAR(25),Personnel_Gps_Location DECIMAL(10,2) NOT NULL,Personnel_Date_Of_Registration TIMESTAMP NOT NULL)"
     ibm_db.exec_immediate(gidconnection,gidpersonnels_sql)
 except:
     print( "Transaction couldn't be completed:" , ibm_db.stmt_errormsg())
 else:
-    print ("Transaction complete.")
+    print ("Transaction complete.")"""
