67a7d2a36606afdb43889ab3271b657de1f74817
Fred-Anziko <74971350+Fred-Anziko@users.noreply.github.com>
2022-08-07 22:25:23 +0300

    Update gidmodal.py

--- gidmodal.py
+++ gidmodal.py
@@ -4,6 +4,7 @@ Created on Fri Jun 11 13:31:34 2021
 
 @author: Anziko Fred
 """
+import ibm_db
 def giddb2connection():
     """connection to the DB2 database for gidweb on IBM cloud foundry"""
     try:
