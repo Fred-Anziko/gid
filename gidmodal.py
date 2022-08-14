5fb2ce53c2376a1c9be83af95967c9f452a130dc
Fred-Anziko <74971350+Fred-Anziko@users.noreply.github.com>
2022-08-14 21:30:21 +0300

    Update gidmodal.py

--- gidmodal.py
+++ gidmodal.py
@@ -22,6 +22,7 @@ gidcursor=gidconnection.cursor()
 """Create personnel tables"""
 try:
     gidcursor.execute("CREATE TABLE personnel(Personnel_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1), Personnel_First_Name VARCHAR(25),Personnel_Middle_Name VARCHAR(25),Personnel_Last_Name VARCHAR(25),Personnel_User_Name VARCHAR(25) NOT NULL,Personnel_Password VARCHAR(25) NOT NULL,Personnel_Tel_No VARCHAR(25),Personnel_Email_Address VARCHAR(25),Personnel_Country VARCHAR(25),Personnel_City_Of_Residence VARCHAR(25),Personnel_DOB DATE,Personnel_Gps_Location_Long DECIMAL(10,2),Personnel_Gps_Location_Lat DECIMAL(10,2),Personnel_Date_Of_Registration TIMESTAMP NOT NULL,Personnel_Facial_Reco BLOB)")
+    gidconnection.commit()
 except:
     print( "personnel table couldn't be completed:")
 else:
@@ -32,6 +33,7 @@ else:
 try:
 
     gidcursor.execute("CREATE TABLE todo_tasks(Task_Id_No INTEGER PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),Personel_Employer_Identity INTEGER NOT NULL,Task_Category VARCHAR(25),Task_Name VARCHAR(25),Task_Description VARCHAR(250),Task_Requirements VARCHAR(250),Task_Gps_Location_Long DECIMAL(10,2),Task_Gps_Location_Lat DECIMAL(10,2),Task_Budget_Amount DECIMAL(10,2),Task_Budget_Currency VARCHAR(25),Task_Date_Of_Post  TIMESTAMP NOT NULL,Task_Deadline DATE);")
+    gidconnection.commit()
 except:
     print( "todo tasks table couldn't be completed:")
 else:
@@ -39,6 +41,7 @@ else:
 #Create expert table
 try:
     gidcursor.execute("CREATE TABLE experts(Personnel_Expert_Id_No INTEGER PRIMARY KEY NOT NULL,Expert_Purpose_And_Desires VARCHAR(250),Expert_Qualifications VARCHAR(250),Expert_Skills VARCHAR(250),Expert_Interests_And_Hobbies VARCHAR(250),Expert_Referee_Name VARCHAR(25),Expert_Referee_Contact VARCHAR(25),Expert_Facial_Reco BLOB);")
+    gidconnection.commit()  
 except:
     print( "expert table couldn't be completed:")
 else:
@@ -47,8 +50,10 @@ else:
 try:
 
     gidcursor.execute("CREATE TABLE done_tasks(Task_Id_No INTEGER PRIMARY KEY NOT NULL,Personnel_Expert_Id_No INTEGER NOT NULL,Date_Task_Done TIMESTAMP NOT NULL);")
+    gidconnection.commit()
 
 except:
     print( "done tasks table couldn't be completed:")
 else:
     print ("done tasks table completed.")
+gidconnection.close()
